158d627ae12a07db06c0ba76d5ccade3b922aa8e
Nicolas Ameghino <nameghino@coto.com.ar>
2013-04-10 11:41:12 -0300

    Initial Commit

---
+++ JSON Tuto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSON Tuto
+//
+//  Created by Nicolas Ameghino on 10/04/13.
+//  Copyright (c) 2013 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
